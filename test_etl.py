b07945c189475be938b5570eaa4c980bef905a20
Jahnavi Maddhuri <jahnavi.maddhuri@gmail.com>
2024-10-06 21:07:50 -0400

    include tests and cicd actions

--- test_etl.py
+++ test_etl.py
@@ -1,5 +1,24 @@
 '''This script is to test the etl.py script'''
 
-def test_x():
-    '''tests if '''
-    assert True
+from mylib.query import create_record, read_db, update_ca, delete_ca
+
+# Test CRUD
+def test_create_record():
+    '''tests if create_record function works as expected'''
+    output = create_record()
+    assert len(output) == 1
+
+def test_read_db():
+    '''tests if read_db function works as expected'''
+    output = read_db()
+    assert len(output) == 5 
+
+def test_update_ca():
+    '''tests if update_ca function works as expected'''
+    output = update_ca()
+    assert len(output) == 1
+
+def test_delete_ca():
+    '''tests if delete_ca function works as expected'''
+    output = delete_ca()
+    assert len(output) == 5
--- test_lib.py
+++
@@ -1,7 +0,0 @@
-'''test functions in lib for accurate functionality'''
-
-# from mylib.lib import 
-
-def test_r():
-    '''test read_zip function'''
-    assert True
